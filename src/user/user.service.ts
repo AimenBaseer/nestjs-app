57ac2579bb9358277ff48206111e2f304542f2c9
AimenBaseer <aimenbaseer@gmail.com>
2022-06-01 14:27:07 +0500

    chore: ipdated add user route

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Post } from '@nestjs/common';
+import { ConflictException, Injectable, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
@@ -11,6 +11,9 @@ export class UserService {
 
   createUser = async (user: IUser) => {
     const saltOrRounds = 10;
+    if(this.getUserByEmail(user.email)){
+        throw new ConflictException('User with this email already exists');
+    }
     const hash = await bcrypt.hash(user.password, saltOrRounds);
     user.password = hash;
     return this.userModel.create(user);
